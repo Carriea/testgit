5a80333b12ef73df32d20f8bd3fd68edd73d6719
‘WSJon’ <2918131134@qq.com>
2019-03-05 14:19:48 +0800

    wsjon的改动

--- src/main/java/com/neusoft/App.java
+++ src/main/java/com/neusoft/App.java
@@ -8,7 +8,7 @@ public class App
 {
     public static void main( String[] args )
     {
-        System.out.println( "王淑娟的改动" );
+        System.out.println( "zhangsan的改动" );
         System.out.println( "李四改动的" );
     }
 }
